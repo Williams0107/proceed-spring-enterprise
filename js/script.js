4ba82ffd0a72e1bbd530006920f8fcd7b2e73eea
Williams0107 <104843851+Williams0107@users.noreply.github.com>
2022-08-19 06:55:22 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,117 @@
+
+
+$(document).ready(function(){
+
+    // wow initiation
+    new WOW().init();
+
+    // navigation bar toggle
+    $('#navbar-toggler').click(function(){
+        $('.navbar-collapse').slideToggle(400);
+    });
+
+    // navbar bg change on scroll
+    $(window).scroll(function(){
+        let pos = $(window).scrollTop();
+        if(pos >= 100){
+            $('.navbar').addClass('cng-navbar');
+        } else {
+            $('.navbar').removeClass('cng-navbar');
+        }
+    });
+
+    // sample video popup
+    $(document).ready(function() {
+        $('.popup-youtube').magnificPopup({
+            disableOn: 700,
+            type: 'iframe',
+            mainClass: 'mfp-fade',
+            removalDelay: 160,
+            preloader: false,
+    
+            fixedContentPos: false
+        });
+    });
+
+    // team carousel 
+    $('.team .owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        autoplay: true,
+        dots: true,
+        nav: false,
+        responsiveClass: true,
+        responsive:{
+            0:{
+                items: 1
+            }, 
+            600:{
+                items: 2
+            },
+            1000:{
+                items: 3
+            }
+        }
+    });
+
+    // faq accordion
+    $('.faq-head').each(function(){
+        $(this).click(function(){
+            $(this).next().toggleClass('show-faq-content');
+            let icon = $(this).children('span').children("i").attr('class');
+
+            if(icon == "fas fa-plus"){
+                $(this).children('span').html('<i class = "fas fa-minus"></i>');
+            } else {
+                $(this).children('span').html('<i class = "fas fa-plus"></i>');
+            }
+        });
+    });
+
+    // testimonial carousel 
+    $('.testimonial .owl-carousel').owlCarousel({
+        loop: true,
+        autoplay: true,
+        dots: true,
+        nav: false,
+        items: 1
+    });
+
+});
+
+
+var crrncy = {'EUR': {'PLN': 4.63, 'USD': 1.02,'NGN':425.0, 'CAD':0.76}, 'USD': {'PLN': 4.63, 'EUR': 0.98,'NGN':416.4, 'CAD':0.78},'NGN':{'EUR':425.0,'PLN':4.63,'USD':416.4,'CAD':325.11},'CAD':{'NGN':323.5, 'EUR':0.76, 'USD':0.78,'PLN':3.59}}
+var btn = document.querySelector('.calculate-btn');
+var baseCurrencyInput = document.getElementById('currency-1');
+var secondCurrencyInput = document.getElementById('currency-2');
+var amountInput = document.getElementById('amount');
+var toShowAmount = document.querySelector('.given-amount');
+var toShowBase = document.querySelector('.base-currency');
+var toShowSecond = document.querySelector('.second-currency');
+var toShowResult = document.querySelector('.final-result');
+
+function convertCurrency(event) {
+  event.preventDefault();
+  var amount = amountInput.value;
+  var from = baseCurrencyInput.value;
+  var to = secondCurrencyInput.value;
+  var result = 0;
+  
+  try{
+    if (from == to){
+      result = amount;
+    } else {
+     result = amount * crrncy[from][to];
+  }
+  }
+  catch(err) {
+    result = amount * (1 / crrncy[to][from]);
+  }
+  
+  toShowAmount.innerHTML = amount;
+  toShowBase.textContent = from + ' = ';
+  toShowSecond.textContent = to;
+  toShowResult.textContent = result; 
+}
+
+btn.addEventListener('click', convertCurrency);
